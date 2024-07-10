7cab9d9113a768c6073fa64a861f575ee3eafb26
ankitevince <ankit+github@evincedev.com>
2024-07-10 07:18:50 +0530

    handle forms

--- daily/form-builder/fb-client/src/App.tsx
+++ daily/form-builder/fb-client/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState, Fragment } from 'react'
+import { useState, Fragment, FormEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Input, { IInput } from './component/inputs/Input'
+import { useRef } from 'react'
 
 const formData: IInput[] = [
 	{
 		type: 'text',
-		placeholder: 'Provide your text'
+		placeholder: 'Username',
+		name: 'username'
+	},
+	{
+		type: 'checkbox',
+		name: 'policy',
+		label: 'Agree to policies'
+	},
+	{
+		type: 'select',
+		name: 'gender',
+		selectOptions: [
+			{ label: 'Male', value: 'male' },
+			{ label: 'Female', value: 'female' }
+		]
 	}
 ]
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const myform = useRef(null);
 
 
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+		if (myform.current) {
+			const form = new FormData(myform.current);
+			const formData = Object.fromEntries(form);
+			console.log(formData);
+		};
+	}
 
-  return (
-    <>
-		<form>
-			{
-				formData.map((inputData, i) => {
-					return <Fragment key={i}>
-						<Input data={inputData}/>
-					</Fragment>
-				})
-			}
-		</form>
-    </>
-  )
+
+	return (
+		<>
+			<form ref={myform} onSubmit={(e: FormEvent) => handleSubmit(e)}>
+				{
+					formData.map((inputData, i) => {
+						return <Fragment key={i}>
+							<Input data={inputData} />
+						</Fragment>
+					})
+				}
+				<select name='abc'>
+					<option value={'def'}>def</option>
+				</select>
+
+				<button>Submit</button>
+			</form>
+		</>
+	)
 }
 
 export default App
--- daily/form-builder/fb-client/src/component/inputs/Input.tsx
+++ daily/form-builder/fb-client/src/component/inputs/Input.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 export interface IInput {
-	type: 'text' | 'checkbox' | 'boolean';
+	type: 'text' | 'checkbox' | 'select';
 	name: string;
-	placeholder?: string;
+	placeholder?: string;	
 	label?: string;
+	selectOptions?: {label: string, value: string}[]
 }
 
-export default function Input({data}: {data: IInput}) {
-  return (
-	<>
-		<label 
-		<input type={data.type} placeholder={data.placeholder || ''}/>
-	</>
-  )
+
+function NormalInputs({ data }: { data: IInput }) {
+	return (
+		<>
+			{data.label && <label htmlFor={data.name}>{data.label}</label>}
+			<input type={data.type} placeholder={data.placeholder || ''} name={data.name} id={data.name} />
+			<br />
+		</>
+	)
+}
+
+function Select({ data }: { data: IInput }) {
+	return (
+		<>
+			{data.label && <label htmlFor={data.name}>{data.label}</label>}
+			<select name={data.name}>
+				{
+					data.selectOptions?.map((options, i) => {
+						return (
+							<Fragment key={i}>
+								<option value={options.value}>{options.label}</option>
+							</Fragment>
+						)
+					})
+				}
+			</select>
+			<br />
+		</>
+	)
+}
+export default function Input({ data }: { data: IInput }) {
+	if (data.type === 'select') return <Select data={data} />;
+	else return <NormalInputs data={data} />;
 }
